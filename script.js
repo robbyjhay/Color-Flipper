41eda3e75b11c2e293dd6ba6907eefd911077677
robbyjhay <jeffersonokikioluwa@gmail.com>
2024-08-24 23:11:06 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+document.getElementById('colorButton').addEventListener('click', function() {
+const randomColor = getRandomColor();
+    document.body.style.backgroundColor = randomColor;
+    document.getElementById('colorCode').textContent = randomColor;
+});
+
+function getRandomColor() {
+    const letters = '0123456789ABCDEF';
+    let color = '#';
+    for (let i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+}
